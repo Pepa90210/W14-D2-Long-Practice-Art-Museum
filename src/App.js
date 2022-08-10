52e67aad8a17c2460733fa8a75bf1d6f01121551
Daniel Hoffmann <danielhoffmann1@gmail.com>
2022-08-09 19:10:26 -0500

    finished part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import harvardArt from './data/harvardArt'
+import GalleryNavigation from './components/GalleryNavigation';
 
 
 function App() {
-  console.log(harvardArt)
+  // console.log(harvardArt)
   return (
-    <h1>Hello from App</h1>
+    <GalleryNavigation galleries={harvardArt.records} />
   );
 }
 
---
+++ src/components/GalleryNavigation/index.js
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import './GalleryNavigation.css';
+
+function GalleryNavigation({ galleries }) {
+  console.log(galleries)
+  return (
+    <nav>
+      <h1>Galleries</h1>
+      <NavLink exact to="/">Home</NavLink> |
+      {galleries.map((gallery) => (
+        <span key={gallery.id}>
+          <NavLink to={`/galleries/${gallery.id}`}>{gallery.name}</NavLink> |
+        </span>
+      ))}
+    </nav>
+  )
+}
+
+export default GalleryNavigation;
